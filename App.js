ee1d03bf63d3342b8bd1ca68282d11929f5b8692
hchiam <hchiam@gmail.com>
2020-01-27 00:41:48 -0500

    Set up repo

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      {/* Not a regular button element because needs certain extra things to be cross-compatible: */}
+      <Button
+        onPress={() => {
+          alert('You tapped the button!');
+        }}
+        title="Press Me"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,23 @@
+# Learning React Native
+
+Just one of the things I'm learning. <https://github.com/hchiam/learning>
+
+You can generate a [dependency graph](https://github.com/hchiam/learning-dependency-cruiser) with `bash show_dep_graph.sh`.
+
+## Setup
+
+```bash
+npm install -g expo-cli
+cd learning-react-native
+npm install
+```
+
+## Development
+
+```bash
+npm start
+# hit the "w" key to run in browser
+# http://192.168.0.165:19006/
+# open developer tools and choose a mobile device
+# when you make changes, the preview will automatically reload
+```
