2e1e93950e17c453dcdb54b2b45dd0f382bebff6
hchiam <hchiam@users.noreply.github.com>
2021-08-17 22:07:21 -0400

    use more Core Components

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  SectionList,
+  ActivityIndicator,
+} from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
@@ -14,6 +23,30 @@ export default function App() {
         }}
         title="Press Me"
       />
+      <TextInput
+        style={{ height: 40, textAlign: "center" }}
+        placeholder="This is a TextInput!"
+        onChangeText={(text) => setText(text)}
+        defaultValue={text}
+      />
+      <SectionList
+        sections={[
+          { title: "Section 1", data: ["Apple", "Banana", "Carrot"] },
+          {
+            title: "Section 2",
+            data: ["Ant", "Bear", "Cat"],
+          },
+        ]}
+        renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.sectionHeader}>{section.title}</Text>
+        )}
+        keyExtractor={(item, index) => index}
+      />
+      <ActivityIndicator />
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="small" color="#0000ff" />
+      <ActivityIndicator size="large" color="#00ff" />
     </View>
   );
 }
@@ -29,6 +62,22 @@ const styles = StyleSheet.create({
     color: "white",
     margin: 20, // '1rem' or '10px' broke it because it expects a number, not strings
     fontSize: 30,
+    textAlign: "center",
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: "bold",
+    backgroundColor: "rgba(247,247,247,1.0)",
+  },
+  item: {
+    backgroundColor: "navy",
+    padding: 10,
+    marginVertical: 8,
+    borderRadius: 5,
     textAlign: "center",
   },
 });
